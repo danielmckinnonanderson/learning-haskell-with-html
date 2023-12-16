cf142b963aacc58bf183c78616103c0be7725775
danielmckinnonanderson <danielmckinnon.anderson@gmail.com>
2023-12-15 20:27:33 -0600

    First couple exercises

---
+++ src/HtmlGenerator.hs
@@ -0,0 +1,34 @@
+module HtmlGenerator () where
+
+main = putStrLn htmlRaw
+
+
+htmlRaw :: String 
+htmlRaw = makeHtml "My Webpage" "Hello world!"
+
+html_ :: String -> String
+html_ = el "html"
+
+head_ :: String -> String
+head_ = el "head"
+
+title_ :: String -> String
+title_ = el "<title>"
+
+body_ :: String -> String
+body_ = el "<body>"
+
+makeHtml :: String -> String -> String
+makeHtml title content = 
+    html_ (head_ (title_ title )) 
+    <> body_ (h1_ "Learning Haskell!") <> p1_ content
+
+el :: String -> String -> String
+el tag content =
+    "<" <> tag <> ">" <> content <> "</" <> tag <> ">"
+
+h1_ :: String -> String
+h1_ = el "h1"
+
+p1_ :: String -> String
+p1_ = el "p"
